16fe9c88f45d252992257ad64fc8d9f50759daae
Tempura <33734673+TempuraLantanis@users.noreply.github.com>
2022-12-06 14:17:17 +0100

    first commit

---
+++ src/CarBuilder.java
@@ -0,0 +1,9 @@
+public interface CarBuilder {
+    public void reset();
+    public void buildBody();
+    public void buildEngine();
+    public void buildGPS();
+    public void buildClimateControl();
+    public void additionalAccessories();
+    public void returnProduct();
+}
---
+++ src/ConcreteCarBuilder.java
@@ -0,0 +1,44 @@
+public class ConcreteCarBuilder implements CarBuilder {
+
+    private String type;
+    private String engine;
+    private int seats;
+    private String climateControl;
+    private S
+
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void buildBody() {
+
+    }
+
+    @Override
+    public void buildEngine() {
+
+    }
+
+    @Override
+    public void buildGPS() {
+
+    }
+
+    @Override
+    public void buildClimateControl() {
+
+    }
+
+    @Override
+    public void additionalAccessories() {
+
+    }
+
+    @Override
+    public void returnProduct() {
+
+    }
+}
